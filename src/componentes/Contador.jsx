2ce6e14faa94cd331292de2e8787fe644bd41dbb
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-10-02 01:05:03 -0300

    Contador terminado

---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+export default function Contador() {
+
+    const [actualizar, setActualizar] = useState(0);
+   
+  return (
+    <div className='contador'>
+        <button onClick={() => setActualizar(Number(actualizar) + 1)} className='btn'><i className="fa-solid fa-chevron-up"></i></button>
+        <input onChange={setActualizar} className='numero-input' type="number" value={actualizar} />
+        <button onClick={() => setActualizar(Number(actualizar) - 1)} className='btn'><i className="fa-solid fa-chevron-down"></i></button>
+    </div>
+  )
+}
