dd907f4a0f51c5a7768595ab192298c9b17e8aa1
Soledad Shukuya <soledadshukuya@hotmail.com>
2024-01-01 20:33:53 -0300

    Contador actualizado

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -2,13 +2,31 @@ import React, {useState} from "react";
 
 export default function Contador() {
 
-    const [actualizar, setActualizar] = useState(0);
+  const [actualizar, setActualizar] = useState(0);
+  
+  const sumar = () => { 
+    setActualizar(Number(actualizar) + 1);
+  }
+
+  const restar = () => {
+    setActualizar(Number(actualizar) - 1);
+  }
+
+  const reset = () => {
+    setActualizar(0);
+  }
    
   return (
     <div className='contador'>
-      <button onClick={() => setActualizar(Number(actualizar) + 1)} className='btn'><i className="fa-solid fa-chevron-up"></i></button>
-        <input onChange={setActualizar} className='numero-input' type="number" value={actualizar} />
-        <button onClick={() => setActualizar(Number(actualizar) - 1)} className='btn'><i className="fa-solid fa-chevron-down"></i></button>
+
+      <h3 className='numero-input'>{actualizar}</h3>
+
+      <div className="btn-group">
+        <button onClick={sumar} className='btn btn-operacion'>+</button>
+        <button onClick={restar} className='btn btn-operacion'>-</button>
+        <button onClick={reset} className='btn btn-reset'>Reset</button>
+      </div>
+      
     </div>
   )
 }
